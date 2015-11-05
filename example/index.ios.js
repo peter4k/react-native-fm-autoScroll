389491ad39da2305ab696d31e789c39731d23344
Yi Wang <yiwang20@gmail.com>
2015-11-04 17:39:27 -0800

    updated iOS example

--- example/index.ios.js
+++ example/index.ios.js
@@ -6,48 +6,52 @@
 
 var React = require('react-native');
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    ScrollView,
+    TextInput,
+    } = React;
 
 var example = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    getInitialState: function () {
+        return {
+            text: ""
+        }
+    },
+    render: function () {
+        return (
+                <ScrollView contentContainerStyle={styles.container}>
+                    <Text>
+                        FM AutoScroll will scroll the scroll view / list view to make the textInput above the keyboard
+                    </Text>
+                    <Text style={{marginTop: 150}}>
+                        When this text input is focused, the scroll view will automatically scroll to the correct position:
+                    </Text>
+                    <TextInput
+                        style={styles.input}
+                        value={this.state.text}
+                        onChangeText={(text)=>this.setState({text: text})}
+                        />
+                </ScrollView>
+        );
+    }
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        padding: 10,
+    },
+    input: {
+        marginTop: 5,
+        height: 32,
+        width: 180,
+        borderWidth: 1
+    }
 });
 
 AppRegistry.registerComponent('example', () => example);
